1931e338b855d4078fde6ff9bb172c3d5eba39fb
krishnapra450-cpu <krishnapra450@gmail.com>
2026-06-30 11:55:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,33 @@ const userInput = document.getElementById("user-input");
 
 const API_URL = "https://ai-chat-backend-zhqr.onrender.com/chat";
 
+let chatHistory = JSON.parse(localStorage.getItem("chatHistory")) || [];
+
+// पुरानी चैट दिखाओ
+window.onload = () => {
+    chatHistory.forEach(chat => {
+        if (chat.type === "user") {
+            chatBox.innerHTML += `<div class="user-message">${chat.text}</div>`;
+        } else {
+            chatBox.innerHTML += `<div class="bot-message">${chat.text}</div>`;
+        }
+    });
+
+    chatBox.scrollTop = chatBox.scrollHeight;
+};
+
 async function sendMessage() {
     const message = userInput.value.trim();
 
     if (message === "") return;
+
+    // User Message
+    chatHistory.push({
+        type: "user",
+        text: message
+    });
+
+    localStorage.setItem("chatHistory", JSON.stringify(chatHistory));
 
     chatBox.innerHTML += `
         <div class="user-message">${message}</div>
@@ -14,6 +37,12 @@ async function sendMessage() {
 
     userInput.value = "";
     chatBox.scrollTop = chatBox.scrollHeight;
+
+    // Typing...
+    const typingDiv = document.createElement("div");
+    typingDiv.className = "bot-message";
+    typingDiv.innerText = "🤖 Typing...";
+    chatBox.appendChild(typingDiv);
 
     try {
         const response = await fetch(API_URL, {
@@ -28,16 +57,29 @@ async function sendMessage() {
 
         const data = await response.json();
 
+        typingDiv.remove();
+
+        chatHistory.push({
+            type: "bot",
+            text: data.reply
+        });
+
+        localStorage.setItem("chatHistory", JSON.stringify(chatHistory));
+
         chatBox.innerHTML += `
             <div class="bot-message">${data.reply}</div>
         `;
 
     } catch (error) {
+
+        typingDiv.remove();
+
         chatBox.innerHTML += `
             <div class="bot-message">
                 ❌ Server se connect nahi ho paaya.
             </div>
         `;
+
         console.error(error);
     }
 
